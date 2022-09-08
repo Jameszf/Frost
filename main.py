a33b447ec0c25a68db50d7f43c02df9454decab3
Jameszf <fzjames2037@gmail.com>
2022-09-08 12:55:01 -0600

    Expanded to 10 by 10 board.

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ from bitarray import bitarray
 pygame.init()
 
 WIN_SIZE = WIN_WIDTH, WIN_HEIGHT = 600, 600
-SHEET_SIZE = SHEET_WIDTH, SHEET_HEIGHT = WIN_WIDTH // 8 * 6, WIN_HEIGHT // 8 * 2
-TILE_SIZE = TILE_WIDTH, TILE_HEIGHT = WIN_WIDTH // 8, WIN_HEIGHT // 8
+BOARD_TILES = 10
+SHEET_ROWS = 2
+SHEET_COLS = 6
+TILE_SIZE = TILE_WIDTH, TILE_HEIGHT = WIN_WIDTH // BOARD_TILES, WIN_HEIGHT // BOARD_TILES
+SHEET_SIZE = (SHEET_COLS * TILE_WIDTH, SHEET_ROWS * TILE_HEIGHT)
 BOARD_KEYS = ["wPawns", "wKnights", "wBishops", "wRooks", "wQueens", "wKings",
 			  "bPawns", "bKnights", "bBishops", "bRooks", "bQueens", "bKings"]
 
@@ -22,12 +25,36 @@ LIGHT_TILE = 240, 214, 181
 
 
 
+class Bitboard:
+	def __init__(self):
+		self.barray = 0
+		self.piece_key = ""
+
+
+	@staticmethod
+	def make_bitboard(pos_list, piece_key):
+		bboard = Bitboard()
+		bboard.piece_key = piece_key
+
+		for pos in pos_list:
+			bboard.setBit(pos)
+
+		return bboard
+
+
+	def flipBit(self, pos): self.barray ^= (1 << pos) # Flip bit at pos.
+	def setBit(self, pos): self.barray |= (1 << pos) # Set bit at pos to be 1.
+	def clearBit(self, pos): self.barray &= ~(1 << pos) # Set bit at pos to be 0.
+	def getBit(self, pos): return self.barray & (1 << pos)
+		
+
+
 def defaultBoard():
 	"""
-	Generates bitarray representation of the starting position of a regular chess game.
+	Generates bitboards of the starting position of a regular chess game.
 
 	INPUT: void.
-	OUTPUT: bitarray<64>.
+	OUTPUT: List<Bitboard>.
 	"""
 
 	piecePos = {
@@ -45,33 +72,20 @@ def defaultBoard():
 		"bKings": [60]
 	}
 	
-	board = {}
-	
-	for key in BOARD_KEYS:
-		barray = bitarray(64, endian="little")
-		barray.setall(0)
-		poses = piecePos[key]
-	
-		for pos in poses:
-			barray[pos] = True;
-		print(barray)
-	
-		board[key] = barray
-
-	return board
+	return {key: Bitboard.make_bitboard(piecePos[key], key) for key in BOARD_KEYS }
 
 
 
 def drawTiles(screen):
 	"""
-	Draws the 8 by 8 board tiles on screen
+	Draws BOARD_TILES by BOARD_TILES checkerboard on screen
 
 	INPUT: screen (pygame display object).
 	OUTPUT: void.
 	"""
 
-	for y in range(8):
-		for x in range(8):
+	for y in range(BOARD_TILES):
+		for x in range(BOARD_TILES):
 			if (x + y) % 2 == 0:
 				color = LIGHT_TILE
 			else:
@@ -83,11 +97,11 @@ def drawTiles(screen):
 
 
 
-def drawPieces(screen, board, sheet):
+def drawPieces(screen, bboards, sheet):
 	"""
 	Draws chess pieces according to board using sprites from sheet
 
-	INPUT: screen (pygame display object), board (bitarray), sheet (pygame sprite object).
+	INPUT: screen (pygame display object), bboards (List<Bitboard>), sheet (pygame sprite object).
 	OUTPUT: void.
 	"""
 
@@ -109,12 +123,11 @@ def drawPieces(screen, board, sheet):
 
 	
 	for key in BOARD_KEYS:
-		if board[key] != 0:
-			for i in range(64):
-				if board[key][i] == 1:
-					x = (i % 8) * TILE_WIDTH
-					y = (7 - i // 8) * TILE_HEIGHT
-					screen.blit(sheet, (x, y), area=spritePos[key])
+		for i in range(BOARD_TILES ** 2):
+			if bboards[key].getBit(i):
+				x = (i % 8) * TILE_WIDTH
+				y = (7 - i // 8) * TILE_HEIGHT
+				screen.blit(sheet, (x, y), area=spritePos[key])
 
 
 
@@ -123,7 +136,7 @@ def drawBoard(screen, board, sheet):
 	Main rendering function of game. Rendering order MATTERS. 
 	LAYERS order: tiles --> pieces.
 
-	INPUT: screen (pygame display object), board (bitarray), sheet (pygame sprite object).
+	INPUT: screen (pygame display object), board (bitboard), sheet (pygame sprite object).
 	OUTPUT: void.
 	"""
 
@@ -155,7 +168,6 @@ def main():
 		
 		screen.fill(GREEN) # Clear previous frame.
 		drawBoard(screen, board, sheet) # Render new frame.
-
 		pygame.display.flip() 
 		clock.tick(60) # Locked to 60 FPS.
 
@@ -163,6 +175,3 @@ def main():
 
 if __name__ == "__main__":
 	main()
-
-
-
